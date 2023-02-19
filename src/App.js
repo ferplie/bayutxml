7beff0b0cdd435e80e0a9a1fb7bfc507ff862ce7
Marie-Elize Venter <ferplie@gmail.com>
2023-02-19 08:53:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+//HIERDIE IS DIE OU APP
+import "./App.css";
+import "./index.css";
+
+//Arrays:
+
+function App() {
+  return <div className="App">Hallo</div>;
+}
+
+export default App;
