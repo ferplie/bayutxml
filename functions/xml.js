620ceaa229382730b1735d7d6c35a3475a89b63a
ferplie <ferplie@gmail.com>
2023-02-19 10:52:51 +0200

    const convert = require("xml-js");
    exports.handler = async (event, context) => {
      // Generate XML data
      const jsonData = { property: "value" }; // Replace with your JSON data
      const xmlData = convert.js2xml(jsonData, {
        compact: true,
        ignoreComment: true,
      });
    
      // Create response
      const response = {
        statusCode: 200,
        headers: {
          "Content-Type": "application/xml",
        },
        body: xmlData,
      };
    
      return response;
    };

--- functions/xml.js
+++ functions/xml.js
@@ -1,1 +1,21 @@
-1676795454651_xml.js
+const convert = require("xml-js");
+
+exports.handler = async (event, context) => {
+  // Generate XML data
+  const jsonData = { property: "value" }; // Replace with your JSON data
+  const xmlData = convert.js2xml(jsonData, {
+    compact: true,
+    ignoreComment: true,
+  });
+
+  // Create response
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/xml",
+    },
+    body: xmlData,
+  };
+
+  return response;
+};
